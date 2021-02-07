f8b73118e168b4f04a7e4c435fe836a986d32508
Leonardo Sousa <leonardolsousa5@gmail.com>
2021-02-07 19:35:28 -0300

    Manipulando arquivos - DELETE

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -32,11 +32,23 @@ class Database {
         const resultado = await this.escreverArquivo(dadosFinal)
         return resultado;
     }
-    
+
     async listar(id) {
         const dados = await this.getDadosArquivo()
         const dadosFiltrados = dados.filter(item =>(id ? (item.id === id) : true))
         return dadosFiltrados
+    }
+    async remover(id) {
+        if(!id) {
+            return await this.escreverArquivo([])
+        }
+        const dados = await this.getDadosArquivo()
+        const indice = dados.findIndex(item => item.id === parseInt(id))
+        if(!indice === -1) {
+            throw Error('O usuario informado não existe')
+        }
+        dados.splice(indice, 1)
+        return await this.escreverArquivo(dados)
     }
 }
 
